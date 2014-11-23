0613ae8d1cce2fc26e81d32400b4788812c1aaff
Shreyash2512 <sgupta2512@gmail.com>
2014-11-23 17:40:16 -0600

    cachematrix file update
    Solution to assignment 2 Coursera

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,54 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m = matrix())
+{
+  i <- NULL # inverse matrix variable
+  
+  set <- function(matrix) #setting the matrix
+  {
+    m <<- matrix
+    i <<- NULL
+  }
+  
+  get <- function() #getting the matrix
+  {
+    m
+  }
+  
+  setInverse <- function(inv) #setting the inverse of the matrix
+  {
+    i <<- inv
+  }
+  
+  getInverse <- function()
+  {
+    i
+  }
+  
+  #list of methods
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
-
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...)
+{
+  m <- x$getInverse() #gets the inverse of matrix
+  
+  if(!is.null(m)) # getting matrix from the Cache
+  {
+    message("getting cached data")
+    return(m)
+  }
+  
+  data <- x$get() #getting matrix from object
+  
+  m <- solve(data) #calculating the inverse 
+  
+  x$setInverse(m) #setting the inverse
+  
+  m # returning the matrix
+  
+  
 }
